e6e90c4899214479583eae5e00374571c4f9c9c5
diieegoo01 <diiego_reyes@live.cl>
2018-08-26 22:39:05 -0300

    new version of dev-server-swarm

---
+++ run.sh
@@ -0,0 +1,8 @@
+docker stack deploy -c hyperledger-zookeeper.yaml hyperledger-zk
+docker stack deploy -c hyperledger-kafka.yaml hyperledger-kafka
+docker stack deploy -c docker-compose.yaml hyperledger-ca-orderer
+docker stack deploy -c hyperledger-couchdb.yaml hyperledger-couchdb
+docker stack deploy -c hyperledger-peer.yaml hyperledger-peer
+
+
+echo "Listo compareee"
