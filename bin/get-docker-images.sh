9e25cfa427f56f31a83348d40f26b3befdf7092b
diieegoo01 <diiego_reyes@live.cl>
2018-08-27 11:01:58 -0300

    new binaries -v 1.1.0

--- bin/get-docker-images.sh
+++ bin/get-docker-images.sh
@@ -7,21 +7,18 @@
 
 # set the default Docker namespace and tag
 DOCKER_NS=hyperledger
-ARCH=amd64
-VERSION=1.2.0-stable
-BASE_DOCKER_TAG=amd64-0.4.10
+ARCH=x86_64
+VERSION=1.1.1
+BASE_DOCKER_TAG=x86_64-0.4.6
 
 # set of Hyperledger Fabric images
-FABRIC_IMAGES=(fabric-peer fabric-orderer fabric-ccenv fabric-tools)
+FABRIC_IMAGES=(fabric-peer fabric-orderer fabric-ccenv fabric-javaenv fabric-kafka fabric-zookeeper \
+fabric-couchdb fabric-tools)
 
 for image in ${FABRIC_IMAGES[@]}; do
   echo "Pulling ${DOCKER_NS}/$image:${ARCH}-${VERSION}"
   docker pull ${DOCKER_NS}/$image:${ARCH}-${VERSION}
 done
 
-THIRDPARTY_IMAGES=(fabric-kafka fabric-zookeeper fabric-couchdb fabric-baseos)
-
-for image in ${THIRDPARTY_IMAGES[@]}; do
-  echo "Pulling ${DOCKER_NS}/$image:${BASE_DOCKER_TAG}"
-  docker pull ${DOCKER_NS}/$image:${BASE_DOCKER_TAG}
-done
+echo "Pulling ${DOCKER_NS}/fabric-baseos:${BASE_DOCKER_TAG}"
+docker pull ${DOCKER_NS}/fabric-baseos:${BASE_DOCKER_TAG}
